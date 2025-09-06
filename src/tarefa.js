75adcbf8c4c4e13793809ec58f5d35daed661a90
ivan <ivan@teste.com>
2025-09-05 21:47:44 -0300

    Correcao

---
+++ src/tarefa.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const multiplicar = (numero1, numero2) => numero1 * numero2;
+const saudacao = (nome) => `Olá ${nome}!`;
+console.log(multiplicar(2, 5));
+console.log(saudacao("Ivan"));
+//# sourceMappingURL=tarefa.js.map
---
+++ src/tarefa.ts
@@ -0,0 +1,6 @@
+const multiplicar = (numero1: number, numero2: number): number => numero1 * numero2;
+
+const saudacao = (nome: string): string => `Olá ${nome}!`;
+
+console.log(multiplicar(2, 5));
+console.log(saudacao("Ivan"));
